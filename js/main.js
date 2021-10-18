7d1c470348ff879226c2fad1a886044eebea5111
Fantast1k <83510907+HSFantast1k@users.noreply.github.com>
2021-10-18 20:47:16 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $(".fairy-tail__slider").slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prew.svg" alt="Prew"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt="Next"></button>',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+    });
 });
